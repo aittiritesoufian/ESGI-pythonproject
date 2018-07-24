c03da3b041c31e613f265ba4d6d6b52443c3e2ac
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2018-07-24 21:31:17 +0200

    update

--- webannonces/annonces/urls.py
+++ webannonces/annonces/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('accueil/', views.home),
     path('annonce/<int:id_annonce>', views.getAnnonce),
     path('list/<str:search>', views.list),
+    path('none/<str:string>', views.none, name="page_none"),
+    path('addition/<int:value>', views.addition, name="addition"),
+    path('date/', views.date_actuelle, name="date"),
 ]
--- webannonces/annonces/views.py
+++ webannonces/annonces/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect
+from datetime import datetime
+
 
 def home(request):
     """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
@@ -9,13 +11,32 @@ def home(request):
     """)
 
 def getAnnonce(request, id_annonce):
-	return HttpResponse("""
+    if id_annonce > 300:
+        return redirect('page_none', string="this is a string")
+    if id_annonce > 200:
+        return redirect(home)
+    if id_annonce > 100:
+        raise Http404
+    return HttpResponse("""
         <h1>Ceci sera une annonce</h1>
         <p>l'annonce récupérer aura pour id: {0} </p>
     """.format(id_annonce))
 
 def list(request, search):
-	return HttpResponse("""
+    return HttpResponse("""
         <h1>Ceci sera une liste d'annonce</h1>
         <p>la liste affichera l'ensemble des annonces, ou seulement celles dont le titre/le contenu contien la chaîne suivante: {0}</p>
     """.format(search))
+
+def none(request, string):
+    return HttpResponse("""
+        <h1>Ceci sera une page vierge</h1>
+        <p>avec comme paramètre: {0}</p>
+    """.format(string))
+
+def addition(request, value):
+    valeur = value*2
+    return render(request, 'annonces/addition.html', locals())
+
+def date_actuelle(request):
+    return render(request, 'annonces/date.html', {'date': datetime.now()})
