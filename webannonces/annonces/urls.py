d214d33f624303e77350d344f0dc76c679db14d5
Pierre TRUCHOT <pierre.truchot2@gmail.com>
2018-07-25 19:41:13 +0200

    clean & update templates

--- webannonces/annonces/urls.py
+++ webannonces/annonces/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('annonce/<int:id_annonce>', views.getAnnonce),
     path('list/', views.list),
     path('list/<str:title>', views.listSearch),
-    path('none/<str:string>', views.none, name="page_none"),
     path('peupleurbdd/', views.peupleur, name="peupleur"),
 ]
