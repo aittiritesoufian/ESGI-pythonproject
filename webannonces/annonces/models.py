50aea718c2b0de5eb522ff3a66e93db1ba45618a
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2018-07-25 16:12:38 +0200

    update model

--- webannonces/annonces/models.py
+++ webannonces/annonces/models.py
@@ -3,16 +3,16 @@ from django.utils import timezone
 
 class Annonces(models.Model):
     titre = models.CharField(max_length=100)
-    auteur = models.CharField(max_length=42)
     contenu = models.TextField(null=True)
     date = models.DateTimeField(default=timezone.now, 
                                 verbose_name="Date de publication")
     montant = models.DecimalField(max_digits=19, decimal_places=5)
-    
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = "annonces"
         ordering = ['date']
-    
+
     def __str__(self):
         return self.titre
 
