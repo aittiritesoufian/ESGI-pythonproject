775cfa3eec35a047b87627dfa82c7ec5b1f6a0de
Pierre TRUCHOT <pierre.truchot2@gmail.com>
2018-07-25 20:16:44 +0200

    setting template for annonces

--- webannonces/annonces/urls.py
+++ webannonces/annonces/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('accueil/', views.home),
-    path('annonce/<int:id_annonce>', views.getAnnonce),
-    path('list/', views.list),
+    path('', views.home, name="home"),
+    path('annonce/<int:id_annonce>', views.getAnnonce, name="single_annonce"),
+    path('list/', views.list, name="liste_annonces"),
     path('list/<str:title>', views.listSearch),
     path('peupleurbdd/', views.peupleur, name="peupleur"),
 ]
