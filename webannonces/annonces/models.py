ce180138abcf30a70ea0f2eabb6db8e19b2feb2d
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2018-07-25 16:02:57 +0200

    add models annonces

--- webannonces/annonces/models.py
+++ webannonces/annonces/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Annonces(models.Model):
+    titre = models.CharField(max_length=100)
+    auteur = models.CharField(max_length=42)
+    contenu = models.TextField(null=True)
+    date = models.DateTimeField(default=timezone.now, 
+                                verbose_name="Date de publication")
+    montant = models.DecimalField(max_digits=19, decimal_places=5)
+    
+    class Meta:
+        verbose_name = "annonces"
+        ordering = ['date']
+    
+    def __str__(self):
+        return self.titre
