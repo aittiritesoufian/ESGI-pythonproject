6483bd1118c9504a53bae2376832ef31bc8e8a1e
Pierre TRUCHOT <pierre.truchot2@gmail.com>
2018-07-25 16:11:22 +0200

    add model user

--- webannonces/annonces/models.py
+++ webannonces/annonces/models.py
@@ -15,3 +15,19 @@ class Annonces(models.Model):
     
     def __str__(self):
         return self.titre
+
+class User(models.Model):
+    """docstring for User"""
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "user"
+        ordering = ['firstname','lastname']
+
+    def __str__(self):
+        name = str(self.firstname)." ".str(self.lastname)
+        return name
