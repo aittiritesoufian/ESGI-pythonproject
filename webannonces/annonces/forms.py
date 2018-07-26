7f5694f85a6895b17a090ff5a2e69a26cb557249
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2018-07-26 09:48:12 +0200

    fixes

--- webannonces/annonces/forms.py
+++ webannonces/annonces/forms.py
@@ -4,7 +4,7 @@ from .models import Annonces
 class AnnonceForm(forms.ModelForm):
     class Meta:
         model = Annonces
-        exclude = ('annonceur')
+        fields = '__all__'
 
 class ConnexionForm(forms.Form):
     username = forms.CharField(label="Nom d'utilisateur", max_length=30)
